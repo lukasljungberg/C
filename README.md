c33d471982c2e86b96feae40335eef3de611f842
pat0 <lukas-ljungberg@outlook.com>
2025-03-11 22:23:52 +0100

    readme

--- README.md
+++ README.md
@@ -8,5 +8,9 @@ Welcome to my collection of cool C/C++ projects! 🏗️💻
 📌 **[Memory Pool](/memory_pool_lib/README.md)** – Custom memory pool implementation.  
 🛠️ *Currently working on dynamic size allocation!*  
 
+### 🔍 String Scanner  
+📌 **[String Scanner](/string_scanner_lib/README.md)** – ASM-powered string scanning for Linux processes.  
+⚡ Supports **regex** and **object address offsets**!  
+
 ### 🏗️ Work in Progress...  
 🔧 More awesome projects coming soon! Stay tuned.  
--- memory_pool_lib/src/MemoryPool.h
+++ memory_pool_lib/src/MemoryPool.h
@@ -1,5 +1,4 @@
 // TODO: fix dynamic size memory-reallocation
-
 #ifndef MEMORY_POOL_H
 #define MEMORY_POOL_H
 #include <sys/mman.h>
@@ -8,6 +7,7 @@
 #include <string>
 #include <pthread.h>
 
+// TODO: fix dynamic memory allocation with new and free
 #define MAX_ENTRIES 100
 #define KEY_SIZE 64
 #define VALUE_SIZE 1024
@@ -16,6 +16,9 @@ struct SharedMemoryHeader {
     bool initialized;
     size_t count;
     pthread_mutex_t lock; // Mutex
+    // TODO: fix dynamic memory allocation with const char ** or char ** look it up 
+    // char* value = new char[VALUE_SIZE]; // Allocate memory for value
+    // Make VALUE_SIZE change depending on size of object/value... have static size for key
     struct Entry {
         char key[KEY_SIZE];
         char value[VALUE_SIZE];
